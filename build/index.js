0c0e8c54b83c34a85ffd7249ebac71b2c03635a1
Paulo Cesar Matos <matospcr0l4@gmail.com>
2026-09-09 13:36:52 -0300

    kkkkk

--- build/index.js
+++ build/index.js
@@ -1,3 +1,3 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+import { readFile } from "node:fs/promises";
+await readFile("src/index.ts");
 console.log("Hello World");
--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,4 @@
+import { readFile } from "node:fs/promises";
+await readFile("src/index.ts");
 declare const console: { log(message: string): void };
 console.log("Hello World");
