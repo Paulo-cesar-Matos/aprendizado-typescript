5ab5e1ecbe96717dba7e6d60429baebb35d3abae
Paulo Cesar Matos <matospcr0l4@gmail.com>
2026-09-11 17:00:16 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,31 @@
-import { readFile } from "node:fs/promises";
-await readFile("src/index.ts");
-declare const console: {
-  log(message?: any, ...optionalParams: any[]): void;
-};
-console.log("Hello, TypeScript!");
+import readline from "node:readline/promises";
+import process from "node:process";
+type resposta = "sim" | "não";
+
+process.stdout.write(
+  "faço aniversário hj, só que não sei minha idade, tem como me ajudar?\n",
+);
+
+async function obterResposta(): Promise<resposta> {
+  const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+  });
+  const resposta = (await rl.question("")) as resposta;
+  rl.close();
+  return resposta;
+}
+
+let resposta = "sim";
+
+async function main() {
+  const resposta = await obterResposta();
+  if (resposta === "sim") {
+    console.log("Que legaaaaaaaaallll!!!!");
+  } else (resposta === "não");
+  {
+    console.log("vc tem exatos 37 segundos para sair da minha casa");
+  }
+}
+
+main();
